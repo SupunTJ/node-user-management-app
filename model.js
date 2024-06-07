1cdf1df0e8dc86376641e2dcafe81c5f31389ba3
SupunTJ <jayaweeraspn@gmail.com>
2024-06-07 21:33:14 +0530

    setting up database models

---
+++ model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; // it is like a plan or bluprint of the database
+
+const userSchema = new Schema({
+  id: Number,
+  name: String,
+});
+
+const User = mongoose.model("User", userSchema); // assigning userSchema to a model called User
+
+module.export = User;
